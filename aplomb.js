1251564f25aabb001ecf0082e976cd0cabd20473
Demarius Chrite <chrite.demarius@gmail.com>
2015-11-14 20:02:32 -0500

    Getting started.

---
+++ aplomb.js
@@ -0,0 +1,14 @@
+/*
+    ___ usage ___ en_US ___
+    usage: node aplomb.js
+    ___ . ___
+
+*/
+
+var Vizsla = require('vizsla')
+var Delta = require('delta')
+
+
+require('arguable')(module, require('cadence')(function (async, options) {
+    console.log('ok')
+}))
