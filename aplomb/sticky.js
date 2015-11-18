e69c94aedeeb5915994c2ddfa4eefeff5e200cda
Demarius Chrite <chrite.demarius@gmail.com>
2015-11-18 13:20:04 -0400

    Scoping sticky scheduler.

--- aplomb/sticky.js
+++ aplomb/sticky.js
@@ -1,5 +1,20 @@
 var cadence = require('cadence')
+var Vizsla = require('vizsla')
 
 function Scheduler (options) {
     this._endpoints = options.endpoints
+    this._agent = new Vizsla
 }
+
+Scheduler.prototype.addEndpoint = cadence(function (async, url) {
+    async(function () {
+        //Scheduler.health(url, async())
+    }, function () {
+        // add to _endpoints
+    })
+})
+
+Scheduler.prototype.checkEndpoints = cadence(function (async) {
+    async.forEach(function (url) {
+    })(this._endpoints)
+})
