dc594ab9065620f231400afb5ba0892ac7dd54ce
Demarius Chrite <chrite.demarius@gmail.com>
2015-11-19 11:24:24 -0500

    Separate endpoints by health.

--- aplomb/sticky.js
+++ aplomb/sticky.js
@@ -2,6 +2,7 @@ var cadence = require('cadence')
 var Vizsla = require('vizsla')
 
 function Scheduler (options) {
+    this._endpoints = { healthy: [], spotty: [], failed: [] }
     this._endpoints = options.endpoints
     this._agent = new Vizsla
 }
@@ -17,4 +18,8 @@ Scheduler.prototype.addEndpoint = cadence(function (async, url) {
 Scheduler.prototype.checkEndpoints = cadence(function (async) {
     async.forEach(function (url) {
     })(this._endpoints)
+})
+
+Scheduler.prototype.health = cadence(function (async, url) {
+    // test an endpoint
 })
