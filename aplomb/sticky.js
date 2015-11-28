4d98a730db8d7c599758ebb4767e63b9bdb6eb69
Demarius Chrite <chrite.demarius@gmail.com>
2015-11-28 17:48:42 -0500

    Keep health and mock post.

--- aplomb/sticky.js
+++ aplomb/sticky.js
@@ -2,21 +2,24 @@ var cadence = require('cadence')
 var Vizsla = require('vizsla')
 
 function Scheduler (options) {
-    this._endpoints = { healthy: [], spotty: [], failed: [] }
+    this._endpoints = {
+        healthy: {},
+        spotty: {},
+        failed: {}
+    }
     this._endpoints = options.endpoints
     this._agent = new Vizsla
 }
 
 Scheduler.prototype.addEndpoint = cadence(function (async, url, health, mock) {
     async(function () {
-        if (arguments.length > 2) {
-            this._agent.fetch({
-                url: health
-                post: mock
-            }, async())
-        }
+        this._agent.fetch({
+            url: health
+            post: mock
+        }, async())
+    }
     }, function () {
-        this._endpoints.healthy.push(url)
+        this._endpoints.healthy[url] = { health: health, mock: mock }
     })
 })
 
