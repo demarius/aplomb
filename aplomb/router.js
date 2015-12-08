c0b2d96afe7579ef0f74b84937472798cdd75814
Demarius Chrite <chrite.demarius@gmail.com>
2015-12-07 20:39:43 -0500

    Match and extract.

--- aplomb/router.js
+++ aplomb/router.js
@@ -3,23 +3,24 @@ var fnv = require('b-tree/benchmark/fnv')
 
 function Router (options) {
     this.delegates = options.delegates
+    this.extract = options.extract
     var length = 256, dist = Math.floor(length / options.delegates.length)
     this.buckets = []
-    this.delegates.map(function (del) {
+    this.delegates.forEach(function (del) {
         for (var i=0; i<dist; i++) {
             length--
             this.buckets.push({
                 url: del
             })
         }
-    }.bind(this))
+    }, this)
     while (length-- > 0) {
         this.buckets.push({ url: this.delegates[this.delegates.length - 1] })
     }
 }
 
-Router.prototype.match = function (key) {
-   return fnv(key, 0, 32).readUIntLE(0, 1)
+Router.prototype.match = function (obj) {
+   return this.buckets[fnv(this.extract(obj), 0, 32).readUIntLE(0, 1)].url
 }
 
 exports.Router = Router
--- t/aplomb/shard/router.t.js
+++ t/aplomb/shard/router.t.js
@@ -1,15 +1,20 @@
-require('proof')(1, prove)
+require('proof')(2, prove)
 
 function prove(assert) {
     var r = new require('../../../aplomb/router.js')
-    var router = new r.Router({
-        delegates: [
+    var delegates = [
             'http://192.168.0.14:8080',
             'http://192.168.0.14:5432',
             'http://192.168.0.14:2345'
         ]
+    var router = new r.Router({
+        delegates: delegates,
+        extract: function (obj) {
+            return 2
+        }
     })
 
-    console.log(router.buckets)
-    assert(router.buckets[120].url, 'http://192.168.0.14:5432', 'true')
+    assert(router.buckets[120].url, delegates[1], 'true')
+
+    assert(delegates.indexOf(router.match({})) > -1, 'delegate matched')
 }
