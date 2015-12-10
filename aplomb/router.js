fc67fb6e6291e1fe050cc016c191d2849bda0877
Demarius Chrite <chrite.demarius@gmail.com>
2015-12-10 10:30:22 -0500

    Move distribution.

--- aplomb/router.js
+++ aplomb/router.js
@@ -2,9 +2,18 @@ var hash = require('hash.murmur3.32')
 var fnv = require('b-tree/benchmark/fnv')
 
 function Router (options) {
-    this.delegates = options.delegates
+    this.distribute(options.delegates)
     this.extract = options.extract
-    var length = 256, dist = Math.floor(length / options.delegates.length)
+}
+
+Router.prototype.match = function (obj) {
+    var key = this.extract(obj)
+    return this.buckets[fnv(new Buffer(key), 0, Buffer.byteLength(key)).readUIntLE(0, 1)].url
+}
+
+Router.prototype.distribute = function (delegates) {
+    var length = 256, dist = Math.floor(length / delegates.length)
+    this.delegates = delegates
     this.buckets = []
     this.delegates.forEach(function (del) {
         for (var i=0; i<dist; i++) {
@@ -17,11 +26,6 @@ function Router (options) {
     while (length-- > 0) {
         this.buckets.push({ url: this.delegates[this.delegates.length - 1] })
     }
-}
-
-Router.prototype.match = function (obj) {
-    var key = this.extract(obj)
-    return this.buckets[fnv(new Buffer(key), 0, Buffer.byteLength(key)).readUIntLE(0, 1)].url
 }
 
 Router.prototype.newDelegate = function (del) {
