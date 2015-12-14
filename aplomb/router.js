e462b0222e269304cc4bc198de7ff1919342e3ac
Demarius Chrite <chrite.demarius@gmail.com>
2015-12-13 19:36:21 -0500

    Rewriting `add` and `remove`.

--- aplomb/router.js
+++ aplomb/router.js
@@ -5,20 +5,17 @@ var monotonic = require('monotonic')
 
 function Router (options) {
     this.routes = []
-    this.distribute(options.delegates, 256, options.version || 0) // switch to
-    //monotonic
+    this.distribute(options.delegates, 256, options.version)
     this.extract = options.extract
-    this.incrementVersion = options.incrementVersion || function (x) {
-        //return  x + 1
-        console.log(x)
+    this.incrementVersion = function (x) {
         return monotonic.increment(x)
     }
-    this.connections = [ this.connectionTable(options.version || 0xffffffff) ]
+    this.connections = [ this.connectionTable(monotonic.parse(options.version)) ]
 }
 
 Router.prototype.connectionTable = function (version) {
-    return { // switch to monotonic
-        version: monotonic.parse(version.toString()),
+    return {
+        version: monotonic.parse(version),
         connections: new RBTree(function (a, b) {
             a = this.extract(a)
             b = this.extract(b)
@@ -27,10 +24,14 @@ Router.prototype.connectionTable = function (version) {
     }
 }
 
-Router.prototype.match = function (obj) {
-    var key = this.extract(obj)
-    return this.routes[0].buckets[fnv(0, new Buffer(key), 0,
-    Buffer.byteLength(key)) & 0xFF].url
+Router.prototype.match = function (connection) {
+    var key = this.extract(connection), delegates = []
+    for (var i = 0, I = this.routes.length; i < I; i++) {
+        delegates.push(this.routes[i].buckets[fnv(0, new Buffer(key), 0, Buffer.byteLength(key)) & 0xFF].url)
+    }
+    return delegates.filter(function (del, i, set) {
+        return (set.indexOf(del) == i)
+    })
 }
 
 Router.prototype.distribute = function (delegates, length, version) {
@@ -53,7 +54,7 @@ Router.prototype.distribute = function (delegates, length, version) {
     this.routes.unshift({
         buckets: buckets,
         delegates: delegates,
-        version: monotonic.parse(version.toString())
+        version: monotonic.parse(version)
     })
 }
 
@@ -106,28 +107,43 @@ Router.prototype.remove = function (delegate) {
 }
 
 Router.prototype.addConnection = function (version, connection) {
-    if (version > this.connections[0].version) {
-        this.connections.unshift(this.connectionTable(version))
+    version = monotonic.parse(version)
+
+    var i = 0, I
+    for (var compare, I = this.connections.length; i < I; i++) {
+        compare = monotonic.compare(version, this.connections[i].version)
+        if (compare == 0) {
+            break
+        } else if (compare > 0) {
+            this.connections.splice(i, 0, this.connectionTable(version))
+            break
+        }
     }
-    if (!this.connections[0].connections.insert(connection)) {
-        //trouble
+    if (i == I) {
+        this.connections.push(this.connectionTable(version))
     }
+    this.connections[i].connections.insert(connection)
 }
 
-Router.prototype.removeConnection = function (version, connection) {
-    if (!this.connections[0].connections.remove(connection)) {
-        //trouble
+Router.prototype.removeConnection = function (connection) {
+   var i=0, indices = []
+   for (var I = this.connections.length; i < I;) {
+        var tree = this.connections[i].connections
+        tree.remove(connection)
+        if (tree.size == 0) {
+            this.connections.splice(i, 1)
+            I--
+        } else { i++ }
     }
 }
 
 Router.prototype.evictable = function (latest, delegate, inspect) {
-    var version, old = this.connections.length
+    var version, old = this.connections.length, conn, iter
     while (old--) {
-        this.connections[old].connections.each(function (conn) {
-            if (delegate == this.match(conn)) {
-                return conn
-            }
-        })
+        iter = this.connection[old].connections.iterator()
+        while ((conn = iter.next()) !== null) {
+            //if match, return
+        }
     }
 }
 
