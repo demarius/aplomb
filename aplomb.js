7b5be8e3d3e4118087c2682d5683f644540b0166
Demarius Chrite <chrite.demarius@gmail.com>
2016-03-17 14:01:39 -0400

    Specify delete count.

--- aplomb.js
+++ aplomb.js
@@ -109,7 +109,7 @@ Router.prototype.removeDelegate = function (delegate) {
 Router.prototype.replaceDelegate = function (oldUrl, newUrl) {
     var delegates = this.routes[0].delegates.slice(),
     buckets = this.routes[0].buckets.slice()
-    delegates = delegates.splice(delegates.indexOf(oldUrl))
+    delegates = delegates.splice(delegates.indexOf(oldUrl), 1)
     delegates.push(newUrl)
 
     for (var b = 0, I = buckets.length; b < I; b++) {
